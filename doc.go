dc8c0d140cc7de9bdddc926bcdf80f28c8a2177e
Isabella Muerte <63051+slurps-mad-rips@users.noreply.github.com>
2021-08-28 20:25:14 -0700

    📝 Add initial documentation for primary module (#2)
    * 📝 Add initial documentation for primary module
    
    ⬆️ Upgrade golang to 1.17
    ✨ Implement Initialize for test.Suite
    ✨ Implement test.Suite.FileResponseHandler
    ✨ Implement test.Suite.Template
    ✨ Implement test.Suite.Render
    ✨ Implement test.Suite.Parameters
    
    Signed-off-by: Isabella Muerte <63051+slurps-mad-rips@users.noreply.github.com>
    
    * 👷 Add check duplicate actions skip step
    
    Signed-off-by: Isabella Muerte <63051+slurps-mad-rips@users.noreply.github.com>
    
    * 👷 Set pull_request types for build workflow
    
    Signed-off-by: Isabella Muerte <63051+slurps-mad-rips@users.noreply.github.com>

--- doc.go
+++ doc.go
@@ -1,1 +1,33 @@
+// Copyright 2021 Isabella Muerte. All rights reserved.
+//
+// Use of this source code is governed by the MIT License that can be found in
+// the LICENSE.md file that accompanied this package
+
+/*
+Doze is a library that wraps the resty golang library to more closely focus its
+API for us in writing small and quick REST wrappers for use in terraform
+provider plugins. Additionally, it provides a test module to make testing the
+REST APIs you write easier when a given platform does *not* provide a sandbox
+API to test against
+
+Usage
+
+The doze package is designed to be used with go modules enabled only.
+
+	import "occult.work/doze"
+	import "occult.work/doze/test"
+
+Examples:
+
+Construct a Client, then get a Request object to perform requests with.
+
+	client := doze.NewClient().
+		SetTimeout(time.Duration(1 * time.Minute)).
+		SetAuthScheme("Basic").
+		SetAuthToken(token).
+		SetHostURL(BaseURL).
+		SetError(&Error{})
+	request = client.Request(ctx, &ReturnType{})
+
+*/
 package doze
