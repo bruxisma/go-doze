c59749617eeab222544fdad1bad45590e78e1536
Isabella Muerte <63051+slurps-mad-rips@users.noreply.github.com>
2021-08-29 14:08:17 -0700

    ✨ Add several Requet method names (#4)
    ♻️ Rename Request.Cast to Request.cast
    
    Signed-off-by: Isabella Muerte <63051+slurps-mad-rips@users.noreply.github.com>

--- request.go
+++ request.go
@@ -1,44 +1,60 @@
 package doze
 
-import "github.com/go-resty/resty/v2"
+import (
+	"context"
+
+	"github.com/go-resty/resty/v2"
+)
 
 type Request resty.Request
 
-// Cast the request to the underlying resty.Request type
-func (request *Request) Cast() *resty.Request {
-	return (*resty.Request)(request)
+func (r *Request) Context() context.Context {
+	return r.cast().Context()
 }
 
 func (r *Request) SetPathParameter(parameter, value string) *Request {
-	return (*Request)(r.Cast().SetPathParam(parameter, value))
+	return (*Request)(r.cast().SetPathParam(parameter, value))
 }
 
 func (r *Request) SetQueryParameter(parameter, value string) *Request {
-	return (*Request)(r.Cast().SetQueryParam(parameter, value))
+	return (*Request)(r.cast().SetQueryParam(parameter, value))
+}
+
+func (r *Request) SetHeader(header, value string) *Request {
+	return (*Request)(r.cast().SetHeader(header, value))
 }
 
 func (r *Request) SetBody(body interface{}) *Request {
-	return (*Request)(r.Cast().SetBody(body))
+	return (*Request)(r.cast().SetBody(body))
+}
+
+func (r *Request) SetContentLength() *Request {
+	return (*Request)(r.cast().SetContentLength(true))
 }
 
 func (request *Request) Get(url string) (interface{}, error) {
-	return checkResponse(request.Cast().Get(url))
+	return checkResponse(request.cast().Get(url))
 }
 
 func (request *Request) Patch(url string) (interface{}, error) {
-	return checkResponse(request.Cast().Patch(url))
+	return checkResponse(request.cast().Patch(url))
 }
 
 func (request *Request) Post(url string) (interface{}, error) {
-	return checkResponse(request.Cast().Post(url))
+	return checkResponse(request.cast().Post(url))
 }
 
 func (request *Request) Put(url string) (interface{}, error) {
-	return checkResponse(request.Cast().Put(url))
+	return checkResponse(request.cast().Put(url))
 }
 
 func (request *Request) Delete(url string) (interface{}, error) {
-	return checkResponse(request.Cast().Delete(url))
+	return checkResponse(request.cast().Delete(url))
+}
+
+// Cast the request to the underlying resty.Request type
+func (request *Request) cast() *resty.Request {
+	return (*resty.Request)(request)
 }
 
 func checkResponse(response *resty.Response, err error) (interface{}, error) {
